7e8362e118d5d98c9ec7c07ac63d2b2df8ba1ea5
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2024-11-25 15:28:19 +0800

    Add files via upload

---
+++ Day 1 - Band Name Generator/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is your favorite pet name? ")
+print("Your band name could be " + city + " " + pet)
+
+
---
+++ Day 1 - Band Name Generator/VariablePractice.py
@@ -0,0 +1,7 @@
+glass1 = "milk"
+glass2 = "juice"
+glass3 = glass2
+glass2 = glass1
+glass1 = glass3
+
+# Goal is to switch the contents of glass1 and glass2
